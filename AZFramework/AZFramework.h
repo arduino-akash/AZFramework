7b88ace05fe72089fa4b333e32f31ba9283d116e
akash.a.singh <113594078+arduino-akash@users.noreply.github.com>
2022-11-25 12:12:37 +0530

    Initial Commit

---
+++ AZFramework/AZFramework.h
@@ -0,0 +1,18 @@
+//
+//  AZFramework.h
+//  AZFramework
+//
+//  Created by Singh, Akash | RIEPL on 25/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AZFramework.
+FOUNDATION_EXPORT double AZFrameworkVersionNumber;
+
+//! Project version string for AZFramework.
+FOUNDATION_EXPORT const unsigned char AZFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AZFramework/PublicHeader.h>
+
+
